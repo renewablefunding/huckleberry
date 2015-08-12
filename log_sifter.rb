4691af563512f2bb523138541f2a3812af89f3b2
Adam <amcfadden@renewfund.com>
2015-08-12 10:56:12 -0700

    refactor to entirely use array parsing, all test passing

--- log_sifter.rb
+++ log_sifter.rb
@@ -5,7 +5,6 @@ require 'english'
 require_relative 'app_helper'
 
 class LogSifter
-  # heredoc USAGE
   def self.show_usage
     puts <<-USAGE
 
@@ -22,7 +21,6 @@ class LogSifter
     end
 
     logfile = var + ".log"
-
 
     count = 0
     File.open(logfile) do |f|
@@ -41,12 +39,8 @@ class LogParser
     @line = line
   end
 
-  def parse
-    LogEntry.new(line)
-  end
-
   def convert_to_array
-    LogArray.new(line)
+    LogEntry.new(line)
   end
 
   private
@@ -55,54 +49,39 @@ class LogParser
 
 end
 
-class LogArray
-  attr_reader :route
+class LogEntry
+  attr_reader :route, :is_server_log, :log_type, :date
 
   def initialize(line)
     @line = line
     @array = line.split(" ")
     @route = nil
+    @is_server_log = nil
+    @log_type = nil
+    @date = nil
   end
 
   def parse_array
     array.each do |element|
       @route = element if element[0] == '/'
+      if server_log?
+        @log_type = element.gsub(/[^a-zA-Z]/, '') if is_log_element?(element)
+        @is_server_log = true
+      else
+        @is_server_log = false
+      end
+      @date = element.gsub(/\[/, '') if element.include?('[')
     end
   end
 
-
   private
-
-  attr_reader :line, :array
-
-end
-
-class LogEntry
-  def initialize(line)
-    @line = line
-  end
-
-  def date
-    date_regexp.match(line).captures.first
+  def is_log_element?(string)
+    !!(string =~ /(GET|POST|PATCH|DELETE|PUT)/)
   end
 
   def server_log?
     !!(line =~ /(GET|POST|PATCH|DELETE|PUT) \//)
   end
 
-  def route
-    if line.include?('/unauthenticated')
-      '/unauthenticated'
-    else
-      '/authentication/create'
-    end
-  end
-
-  private
-
-  attr_reader :line
-
-  def date_regexp
-    Regexp.new(/\[(.*)\]/)
-  end
+  attr_reader :line, :array
 end
--- log_sifter_spec.rb
+++ log_sifter_spec.rb
@@ -5,77 +5,82 @@ require_relative 'spec_helper'
 
 RSpec.describe LogSifter do
   it 'will return the log entry object' do
-    # string literal %q
     line = %q(I, [2015-08-10T11:50:43.088546 #13699]  INFO -- : (0.000192s) SELECT 1 AS "one" FROM "sessions" WHERE (sid = "6af5e056a9ed607dd0b") LIMIT 1)
-    log_entry = LogParser.new(line).parse
+    log_entry = LogParser.new(line).convert_to_array
+    log_entry.parse_array
     expect(log_entry).to be_a_kind_of LogEntry
   end
 
   describe "Date parse testing" do
     it 'will return the date of a log entry' do
       line = %q(I, [2015-08-10T11:50:43.088546 #13699]  INFO -- : (0.000192s) SELECT 1 AS "one" FROM "sessions" WHERE (sid = "6af5e056a9ed607dd0b") LIMIT 1)
-      log_entry = LogParser.new(line).parse
-      expect(log_entry.date).to eq('2015-08-10T11:50:43.088546 #13699')
+      log_entry = LogParser.new(line).convert_to_array
+      log_entry.parse_array
+      expect(log_entry.date).to eq('2015-08-10T11:50:43.088546')
     end
 
     it 'will return the date of a log entry' do
       line = %q(I, [2017-08-10T11:50:43.088546 #13699]  INFO -- : (0.000192s) SELECT 1 AS "one" FROM "sessions" WHERE (sid = "6af5e056a9ed607dd0b") LIMIT 1)
-      log_entry = LogParser.new(line).parse
-      expect(log_entry.date).to eq('2017-08-10T11:50:43.088546 #13699')
+      log_entry = LogParser.new(line).convert_to_array
+      log_entry.parse_array
+      expect(log_entry.date).to eq('2017-08-10T11:50:43.088546')
     end
 
     it 'will return the date of a log entry' do
       line = %q(I, [2017-07-10T11:51:43.088546 #13699]  INFO -- : (0.000192s) SELECT 1 AS "one" FROM "sessions" WHERE (sid = "6af5e056a9ed607dd0b") LIMIT 1)
-      log_entry = LogParser.new(line).parse
-      expect(log_entry.date).to eq('2017-07-10T11:51:43.088546 #13699')
+      log_entry = LogParser.new(line).convert_to_array
+      log_entry.parse_array
+      expect(log_entry.date).to eq('2017-07-10T11:51:43.088546')
     end
   end
 
   describe "LogParser will label logs as either server logs or database logs" do
     it "is NOT a server log" do
       line = %q(I, [2017-07-10T11:51:43.088546 #13699]  INFO -- : (0.000192s) SELECT 1 AS "one" FROM "sessions" WHERE (sid = "6af5e056a9ed607dd0b") LIMIT 1)
-      log_entry = LogParser.new(line).parse
-      expect(log_entry.server_log?).to be(false)
+      log_entry = LogParser.new(line).convert_to_array
+      log_entry.parse_array
+      expect(log_entry.is_server_log).to be(false)
     end
 
     it "is NOT a server log" do
       line = %q(I, [2017-07-10T11:51:43.088546 #13699]  INFO -- : (0.000192s) SELECT 1 AS "one" FROM "sessions" WHERE (sid = "GETasdlkjo3as") LIMIT 1)
-      log_entry = LogParser.new(line).parse
-      expect(log_entry.server_log?).to be(false)
+      log_entry = LogParser.new(line).convert_to_array
+      log_entry.parse_array
+      expect(log_entry.is_server_log).to be(false)
     end
 
     %w(PATCH POST GET DELETE PUT).each do |http_verb|
       it "is a server log if it contains the http verb #{http_verb}" do
         line = %Q(127.0.0.1 - - [10/Aug/2015:11:50:43 -0700] "#{http_verb} /authentication/create " 302 - 0.0684)
-        log_entry = LogParser.new(line).parse
-        expect(log_entry.server_log?).to be(true)
+        log_entry = LogParser.new(line).convert_to_array
+        log_entry.parse_array
+        expect(log_entry.is_server_log).to be(true)
       end
     end
   end
 
-  describe "Log Type parse testing" do
-    xit 'will return the log type of a log entry' do
-      line = %q(I, [2017-07-10T11:51:43.088546 #13699]  INFO -- : (0.000192s) SELECT 1 AS "one" FROM "sessions" WHERE (sid = "6af5e056a9ed607dd0b") LIMIT 1)
-      log_entry = LogParser.new(line).parse
-      expect(log_entry.type).to eq('INFO')
-    end
-
-    pending 'will return the log type of a log entry' do
-      line = %q(127.0.0.1 - - [10/Aug/2015:14:20:43 -0700] "GET / " 302 - 0.0176)
-      log_entry = LogParser.new(line).parse
-      expect(log_entry.type).to eq('GET')
+  describe "Returning the log type of server logs" do
+    %w(PATCH POST GET DELETE PUT).each do |http_verb|
+      it "will return the log type of a server log entry if it is #{http_verb}" do
+        line = %Q(127.0.0.1 - - [10/Aug/2015:14:20:43 -0700] "#{http_verb} / " 302 - 0.0176)
+        log_entry = LogParser.new(line).convert_to_array
+        log_entry.parse_array
+        expect(log_entry.log_type).to eq("#{http_verb}")
+      end
     end
   end
 
   describe "Log route parse testing" do
     it 'will return the log route of a log entry' do
       line = '127.0.0.1 - - [10/Aug/2015:14:20:43 -0700] "GET /unauthenticated " 200 - 0.0063'
-      log_entry = LogParser.new(line).parse
+      log_entry = LogParser.new(line).convert_to_array
+      log_entry.parse_array
       expect(log_entry.route).to eq('/unauthenticated')
     end
     it 'will return the log route of a log entry' do
       line = '127.0.0.1 - - [10/Aug/2015:11:50:43 -0700] "POST /authentication/create " 302 - 0.0684'
-      log_entry = LogParser.new(line).parse
+      log_entry = LogParser.new(line).convert_to_array
+      log_entry.parse_array
       expect(log_entry.route).to eq('/authentication/create')
     end
     it 'will return the log route of a log entry' do

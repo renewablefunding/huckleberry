3b455fb7e817c7764757d0e1fdb55604145eaa4c
Adam <amcfadden@renewfund.com>
2015-08-13 12:36:38 -0700

    extract log_entry to separate file, add dupe checker

---
+++ lib/log_duplicate_checker.rb
@@ -0,0 +1,8 @@
+# LogDuplicateChecker will receive chunks of an entire logfile and check for duplicate logs
+# Need to also check the last line of previous log against the first line of the next log
+
+class LogDuplicateChecker
+  def initialize(logfile)
+
+  end
+end

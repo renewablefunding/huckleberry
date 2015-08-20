ced0a63803db4abe577d1c134d06fe1c21797fff
Adam <amcfadden@renewfund.com>
2015-08-20 12:55:32 -0700

    added basic method to grab all related info for errors

--- lib/huckleberry/prod_log_parse.rb
+++ lib/huckleberry/prod_log_parse.rb
@@ -2,12 +2,16 @@
 
 module Huckleberry
   class ProdLogParse
-    attr_reader :important_logs, :counts
+    attr_reader :important_logs
     def initialize(log)
       @log = log
       @important_logs = []
+      @important_processes = {}
       @counts = {}
       parse_log
+      create_important_processes_hash
+      store_all_logs_that_are_important_processes
+      binding.pry
     end
 
     def headline_output
@@ -30,6 +34,10 @@ module Huckleberry
 
         #{counts["500"].to_s} 500's found
 
+        #{counts["error"].to_s} ERROR's found
+
+        #{counts["fatal"].to_s} FATAL's found
+
 - - - - - - - - - - - - - - - - - - - - - - - - - -
       OUTPUT
     end
@@ -44,24 +52,85 @@ Huckleberry Log Report for #{DateTime.now.to_s} is attached.
     end
 
     private
-    attr_reader :log
+    attr_reader :log, :counts, :important_processes
 
     def parse_log
       count_404 = 0
       count_500 = 0
+      count_error = 0
+      count_fatal = 0
       log.each_line.each_with_index do |raw_line, index|
         raw_line.strip!
         line = raw_line.split(" : ")[1]
         next if line.nil?
-        next if line =~ (/ 302 /)
-        next if line =~ (/ 200 /)
-        next if line =~ (/Rendered/)
-        next if line =~ (/Param/)
+        # next if line =~ (/ 302 /)
+        # next if line =~ (/ 200 /)
+        # next if line =~ (/Rendered/)
+        # next if line =~ (/Param/)
         important_logs << (index.to_s + raw_line) && count_404 += 1 if line =~ (/ 404 /)
         important_logs << (index.to_s + raw_line) && count_500 += 1 if line =~ (/ 500 /)
+        important_logs << (index.to_s + raw_line) && count_error += 1 if line =~ (/ERROR/)
+        important_logs << (index.to_s + raw_line) && count_fatal += 1 if line =~ (/FATAL/)
       end
       counts["404"] = count_404
       counts["500"] = count_500
+      counts["error"] = count_error
+      counts["fatal"] = count_fatal
+      log.rewind
+    end
+
+    def create_important_processes_hash
+      important_logs.each do |log_line|
+        pid = log_line.match(/#[\d]*/)
+        important_processes[pid.to_s] = [] unless important_processes.has_key?(pid)
+      end
+    end
+
+    def store_all_logs_that_are_important_processes
+      log_array = log.readlines
+      last_completed_line = 0
+      last_with_404 = 0
+      important_processes.each_key do |pid|
+        log_array.each_with_index do |line, index|
+          next unless line.include?(pid)
+          line.strip!
+
+          if line =~ (/Started/)
+            foo = false
+            count = 1
+            started_at = index
+            until foo
+              look_ahead_line = started_at + count
+              if look_ahead_line >= log_array.length || log_array[look_ahead_line].nil?
+                foo = true
+                next
+              end
+              if !log_array[look_ahead_line].include?(pid)
+                # binding.pry
+                count += 1
+                next
+              elsif log_array[look_ahead_line] =~ (/Completed 404/)
+                count += 1
+                task = log_array.slice(started_at..look_ahead_line)
+                task.select! {|line| line.include?pid}
+                important_processes[pid] << task
+                puts index
+                foo = true
+              elsif log_array[look_ahead_line] =~ (/Completed/)
+                foo = true
+                count += 1
+                puts index.to_s + "uno"
+              else
+                count += 1
+              end
+            end
+          end
+        end
+      end
     end
   end
 end
+
+
+# hash.has_key?("PID")
+# hash[:PID] = []
